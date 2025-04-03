d27e0e2d1e81cece289f93ce344e24ce0294d521
viniciussilva240 <vinicius.costa@estudante.ifro.edu.br>
2025-04-03 17:08:34 -0400

    03/04/2025

--- aula-01.js
+++ aula-01.js
@@ -8,3 +8,12 @@ console.log (text.toUpperCase()); //converte o texto para maiúsculas
 console.log('tipo:' + typeof text);// Retorna o tipó da variável
 console.log (boleando.toString()); //converte para texto
 console.log('tipo:' + typeof boleando) //Retorna o tipo da variável
+
+let estados = ["São Paulo", "Rondônia", "Minas Gerais", "Acre"]
+let marcaCarros = [];
+marcaCarros.push  ('fiat')
+console.log(estados[2])
+console.log(marcaCarros[0])
+console.log('tamanho: ' + estados.length)// contar a quantidades de itens dentro de uma lista
+console.log('total de letras: '+ marcaCarros[0].length) //conctenação
+console.log(`Estado: ${estados[1]}`); //interpolação
