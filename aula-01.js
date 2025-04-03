b4058dcd8e90aeffd6f3549e58057a4c8e74a946
viniciussilva240 <vinicius.costa@estudante.ifro.edu.br>
2025-04-03 16:36:18 -0400

    03/04/2025

--- aula-01.js
+++ aula-01.js
@@ -2,6 +2,9 @@ let valor = 20.250000;
 let text= "Meu primeiro projeto em javaScript"
 let boleando = true;
 
-console.log (valor.toFixed(2));
-console.log (text.toUpperCase());
-console.log (boleando.toString());
+console.log (valor.toFixed(2)); //aredonda o valor para duas casas decimais
+console.log('tipo:' + typeof valor); //Retorna o tipo da variável
+console.log (text.toUpperCase()); //converte o texto para maiúsculas
+console.log('tipo:' + typeof text);// Retorna o tipó da variável
+console.log (boleando.toString()); //converte para texto
+console.log('tipo:' + typeof boleando) //Retorna o tipo da variável
